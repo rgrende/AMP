2251108700cca797a50e64817e09d9dbdcba0969
roberthereth <rhereth@carroll.edu>
2023-03-02 11:58:40 -0700

    Fixed database tester

--- src/ampcc/com/AMP_H2.java
+++ src/ampcc/com/AMP_H2.java
@@ -7,12 +7,16 @@ public class AMP_H2 {
     }
 
     public static void test() {
-        final String DB_URL="jdbc:h2:file:..\\data\\db";
+
+        final String DB_URL="jdbc:h2:file:..\\data\\db"; // Establishes location of db
 
         try {
+            // Connection and Statement allow use of DB within Java
             Connection conn = DriverManager.getConnection(DB_URL);
             Statement stat = conn.createStatement();
 /*
+            Testing to see if we can execute SQL to build database within code, use for
+            first time setup and updating tables with info.
 
             stat.execute("CREATE TABLE Song(id NUMERIC (31) PRIMARY KEY ,\n" +
                     "    artist_id NUMERIC (31) NOT NULL ,\n" +
@@ -29,11 +33,12 @@ public class AMP_H2 {
             stat.execute("INSERT INTO Song(id,artist_id,\"name\",length,\"year\",\"file\") VALUES\n" +
                     "    (1,1,'DO IT ALL THE TIME',167,2018,'songfilepath')");
 */
+            // Holds results of sql query
             ResultSet results = stat.executeQuery("SELECT * FROM Artist");
 
             System.out.println("Reading Artist table...");
             while (results.next()) {
-                System.out.println(results);
+                System.out.println("Name: " + results.getString("name"));
             }
 
             stat.close();
