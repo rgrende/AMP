5ef93c1b54aa8edb84dc06b5f8d8e3eaaee2a9a5
roberthereth <rhereth@carroll.edu>
2023-03-02 00:11:26 -0700

    Added H2 test piece and jar

--- src/ampcc/com/AMP_H2.java
+++ src/ampcc/com/AMP_H2.java
@@ -12,6 +12,22 @@ public class AMP_H2 {
         try {
             Connection conn = DriverManager.getConnection(DB_URL);
             Statement stat = conn.createStatement();
+
+            stat.execute("CREATE TABLE Song(id NUMERIC (31) PRIMARY KEY ,\n" +
+                    "    artist_id NUMERIC (31) NOT NULL ,\n" +
+                    "    name VARCHAR NOT NULL ,\n" +
+                    "    length NUMERIC NOT NULL ,\n" +
+                    "    year NUMERIC (4) ,\n" +
+                    "    file VARCHAR NOT NULL)");
+            stat.execute("CREATE TABLE Artist\n" +
+                    "(id NUMERIC (31) PRIMARY KEY ,\n" +
+                    "name VARCHAR)");
+
+            stat.execute("INSERT INTO Artist(id,\"name\") VALUES\n" +
+                    "    (1, 'I DONT KNOW HOW BUT THEY FOUND ME')");
+            stat.execute("INSERT INTO Song(id,artist_id,\"name\",length,\"year\",\"file\") VALUES\n" +
+                    "    (1,1,'DO IT ALL THE TIME',167,2018,'songfilepath')");
+
             ResultSet results = stat.executeQuery("SELECT * FROM Song");
 
             System.out.println("Reading Songs table...");
