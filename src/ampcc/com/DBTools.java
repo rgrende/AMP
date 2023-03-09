aec60cd7d47fc88f18e591a0b19587923b9b1e94
roberthereth <rhereth@carroll.edu>
2023-03-09 14:00:39 -0700

    Added additional methods to DBTools

--- src/ampcc/com/DBTools.java
+++ src/ampcc/com/DBTools.java
@@ -7,48 +7,60 @@ public class DBTools {
 
     public static void main(String[] args) {
         DBTools t = new DBTools();
+
+        System.out.println("All Songs:");
         t.readSongs();
+        System.out.println();
+
+        System.out.println("All Artists:");
+        t.readArtists();
+        System.out.println();
     }
 
     public void readSongs() {
-        try {
-            Connection conn = DriverManager.getConnection(this.DB_URL);
-            Statement stat = conn.createStatement();
-            String line = "SELECT * FROM Song";
-            ResultSet results = stat.executeQuery(line);
-            printResults(results);
-            stat.close();
-            conn.close();
-        } catch (Exception ex) {
-            System.out.println("ERROR: " + ex.getMessage());
-        }
+        String line = "SELECT * FROM Song";
+        runLine(line);
     }
 
     public void readArtists() {
-        try {
-            Connection conn = DriverManager.getConnection(this.DB_URL);
-            Statement stat = conn.createStatement();
-            String line = "SELECT * FROM Artist";
-            ResultSet results = stat.executeQuery(line);
-            printResults(results);
-            stat.close();
-            conn.close();
-        } catch (Exception ex) {
-            System.out.println("ERROR: " + ex.getMessage());
-        }
+        String line = "SELECT * FROM Artist";
+        runLine(line);
     }
 
     public void readTagSongs(String t_id) {
+        String line = "SELECT * FROM Song s, SongTag st WHERE s.id = st.song_id " +
+                " AND st.Tag_id = " + t_id + ";";
+        runLine(line);
+
+    }
+
+    private Object[] arrayResults(ResultSet r) {
         try {
-            Connection conn = DriverManager.getConnection(this.DB_URL);
-            Statement stat = conn.createStatement();
-            String line = "SELECT * FROM Song s, SongTag st WHERE s.id = st.song_id " +
-                    " AND st.Tag_id = " + t_id + ";";
-            ResultSet results = stat.executeQuery(line);
-            printResults(results);
-        } catch(Exception ex){
+            ResultSetMetaData rmd = r.getMetaData();
+            int columnCount = rmd.getColumnCount();
+
+            String[] colNames = new String[columnCount];
+            String[][] data = new String[r.getRow()][columnCount];
+
+            for (int i = 1; i <= columnCount; i++) {
+                colNames[i-1] = rmd.getColumnName(i);
+            }
+            int rowCount = 0;
+            while (r.next()) {
+                Object[] values = new Object[columnCount];
+                for (int i = 1; i <= columnCount; i++) {
+                    values[i - 1] = r.getObject(i);
+                }
+                for (int item = 0; item < values.length; item++) {
+                    data[rowCount][item] = values[item].toString();
+                }
+                rowCount++;
+            }
+            return new Object[]{colNames, data};
+        } catch (Exception ex){
             System.out.println("ERROR: " + ex.getMessage());
         }
+        return null;
     }
 
     private void printResults(ResultSet r) {
@@ -74,6 +86,19 @@ public class DBTools {
                 System.out.println();
             }
         } catch (Exception ex){
+            System.out.println("ERROR: " + ex.getMessage());
+        }
+    }
+
+    private void runLine(String line) {
+        try {
+            Connection conn = DriverManager.getConnection(this.DB_URL);
+            Statement stat = conn.createStatement();
+            ResultSet results = stat.executeQuery(line);
+            printResults(results);
+            stat.close();
+            conn.close();
+        } catch(Exception ex){
             System.out.println("ERROR: " + ex.getMessage());
         }
     }
